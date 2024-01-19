c557bc257efa45015177af21682883f7e17421d2
Felix sindani <felix.sindani@gmail.com>
2024-01-19 18:30:27 +0000

    fix pep8

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""recursive function that queries the Reddit API and returns
- a list containing the titles of all hot articles for a given subreddit. """
+"""recursive function that queries the Reddit API """
 
 import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
-    """ returns a list containing the titles of all hot articles for a given subreddit. If no results are found for the given subreddit, the function should return None."""
+    """ returns a list containing the title."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
