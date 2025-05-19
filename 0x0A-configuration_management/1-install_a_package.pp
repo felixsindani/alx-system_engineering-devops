351f86c9c4e7542beb16ad7f466e4c2153391890
felix sindani <felixsindani72@gmail.com>
2025-05-19 09:38:48 +0300

    installed flask using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
-#Installs puppet-lint
-
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+#installs flask
+exec { 'install_flask_2.1.0':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  path    => ['/usr/bin', '/bin'],
 }
+
